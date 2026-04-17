d3230cb6fcf0394b84f291b7d82a5ad624eb635e
bom0320 <bom0320.dev@gmail.com>
2026-04-17 09:14:41 +0900

    add: babel.config.js

---
+++ apps/mobile/babel.config.js
@@ -0,0 +1,22 @@
+module.exports = function (api) {
+  api.cache(true);
+
+  return {
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["./"],
+          alias: {
+            "@": ".",
+            "@hyoit/auth": "../../packages/auth",
+            "@hyoit/storage": "../../packages/storage",
+            "@hyoit/types": "../../packages/types",
+            "@hyoit/ui": "../../packages/ui",
+          },
+        },
+      ],
+    ],
+  };
+};
