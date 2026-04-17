34425cc629d2119fcfda05fcee5e798810113c55
bom0320 <bom0320.dev@gmail.com>
2026-04-17 09:47:33 +0900

    fix: childHomePage

--- apps/mobile/src/child/pages/ChildHomePage.tsx
+++ apps/mobile/src/child/pages/ChildHomePage.tsx
@@ -1,3 +1,23 @@
+import { StyleSheet, Text, View } from "react-native";
+
 export default function ChildHomePage() {
-  return <div>Child Home</div>;
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Child Home</Text>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#FFFFFF",
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: "600",
+    color: "#000000",
+  },
+});
